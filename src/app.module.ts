b45672f3050fe44b7de45e7b70e4ab8df3914aa9
Abdullah <abdullah.faisal@koderlabs.com>
2024-02-23 03:12:24 +0500

    feat: .env validator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { I18nModule, HeaderResolver } from 'nestjs-i18n';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { I18nModule, HeaderResolver } from 'nestjs-i18n';
 import * as path from 'path';
 import { FALLBACK_LANGUAGE, LOCALE_HEADER_KEY } from 'constant';
+import { validateEnv } from 'configs/validate-env';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ validate: validateEnv }),
     I18nModule.forRoot({
       fallbackLanguage: FALLBACK_LANGUAGE,
       loaderOptions: {
---
+++ src/configs/validate-env.ts
@@ -0,0 +1,30 @@
+import { plainToInstance } from 'class-transformer';
+import { IsNumber, IsString, validateSync } from 'class-validator';
+
+enum Environment {
+  Development = 'development',
+  Production = 'production',
+  Test = 'test',
+  Provision = 'provision',
+}
+
+class EnvironmentVariables {
+  //   @IsEnum(Environment)
+  NODE_ENV: Environment;
+
+  @IsNumber()
+  PORT: number;
+
+  @IsString()
+  SWAGGER_PASSWORD: string;
+}
+
+export function validateEnv(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, { enableImplicitConversion: true });
+  const errors = validateSync(validatedConfig, { skipMissingProperties: false });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+  return validatedConfig;
+}
